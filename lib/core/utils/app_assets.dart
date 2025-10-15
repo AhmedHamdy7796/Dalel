744d11020dcd0e6068454decb20f074f12d78134
Ahmed Hamdy <93158682+AhmedHamdy7796@users.noreply.github.com>
2025-10-15 01:50:00 -0700

    first commit

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForgetPasword
+  /// assets/images/forgetPasword.png
+  static const String assetsImagesForgetPasword = "assets/images/forgetPasword.png";
+
+  /// Assets for assetsImagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String assetsImagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for assetsImagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String assetsImagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for assetsImagesOnBoarding3
+  /// assets/images/onBoarding3.png
+  static const String assetsImagesOnBoarding3 = "assets/images/onBoarding3.png";
+
+  /// Assets for assetsImagesSucccesOrder
+  /// assets/images/succcesOrder.png
+  static const String assetsImagesSucccesOrder = "assets/images/succcesOrder.png";
+
+  /// Assets for assetsImagesVector1
+  /// assets/images/Vector1.svg
+  static const String assetsImagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for assetsImagesVector2
+  /// assets/images/Vector2.svg
+  static const String assetsImagesVector2 = "assets/images/Vector2.svg";
+}
+
