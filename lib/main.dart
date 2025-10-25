5b9611c46b73d10104705eb708efafd0f9fbfbd9
Ahmed Hamdy <93158682+AhmedHamdy7796@users.noreply.github.com>
2025-10-24 18:36:40 -0700

    fix conflict markers, keep my local code only

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:dalel/core/routes/app_router.dart';
-<<<<<<< Updated upstream
-=======
 import 'package:dalel/core/utils/app_colors.dart';
-import 'package:dalel/core/services/service_locator.dart';
->>>>>>> Stashed changes
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,13 +12,10 @@ class Dalel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-<<<<<<< Updated upstream
-=======
       theme: ThemeData(scaffoldBackgroundColor: AppColors.offWhite),
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors.offWhite,
       ),
->>>>>>> Stashed changes
       debugShowCheckedModeBanner: false,
       routerConfig: router,
     );
